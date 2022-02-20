7bc7a85d3587712090f5c81eba30035300ce58ff
James Riggleman <james.riggleman@yahoo.com>
2022-02-20 12:58:37 +0800

    repeat string done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+// return(num < 0) ? new Error("Error") : str.repeat(num);
+if (num > 0){
+  return str.repeat(num);
+} else if (num < 0){
+  return "ERROR";
+} else{
+  return "";
+}
 };
 
 // Do not edit below this line
